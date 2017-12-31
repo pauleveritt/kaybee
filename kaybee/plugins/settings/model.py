d00409f8103910455f0485b3dee25f97c47b868f
Paul Everitt <pauleveritt@me.com>
2017-12-31 14:10:15 -0500

    @wip Fully-qualified import

--- kaybee/plugins/settings/model.py
+++ kaybee/plugins/settings/model.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 
-from plugins.debugdumper.model import DebugdumperModel
+from kaybee.plugins.debugdumper.model import DebugdumperModel
 
 
 class KaybeeSettings(BaseModel):
