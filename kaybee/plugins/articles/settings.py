8d9806b2ee5bcff09fa188fed19dac349678f6cf
Paul Everitt <pauleveritt@me.com>
2018-02-16 08:50:42 -0500

    @WIP Provide 3 articles settings for flavors of dates.

--- kaybee/plugins/articles/settings.py
+++ kaybee/plugins/articles/settings.py
@@ -3,6 +3,9 @@ from pydantic import BaseModel
 
 class ArticlesModel(BaseModel):
     use_toctree: bool = False
+    datefmt_short: str = '%b %d'
+    datefmt_long: str = '%Y/%m/%d'
+    datefmt_full: str = '%Y/%m/%d %H:%M'
 
     class Config:
         ignore_extra = False
--- tests/unit/plugins/articles/test_articles_settings.py
+++ tests/unit/plugins/articles/test_articles_settings.py
@@ -1,4 +1,14 @@
+from datetime import datetime
+
+import pytest
+
 from kaybee.plugins.articles.settings import ArticlesModel
+
+
+@pytest.fixture()
+def past_datetime() -> datetime:
+    past = datetime(2012, 4, 25, 13, 26)
+    yield past
 
 
 class TestArticlesSettings:
@@ -8,3 +18,27 @@ class TestArticlesSettings:
     def test_construction(self):
         am = ArticlesModel(**dict(use_toctree=False))
         assert False is am.use_toctree
+
+    def test_datefmt_short_default(self, past_datetime: datetime):
+        am = ArticlesModel(**dict())
+        fmt = am.datefmt_short
+        result = past_datetime.strftime(fmt)
+        assert 'Apr 25' == result
+
+    def test_datefmt_short_custom(self, past_datetime: datetime):
+        am = ArticlesModel(**dict(datefmt_short='%b/%d'))
+        fmt = am.datefmt_short
+        result = past_datetime.strftime(fmt)
+        assert 'Apr/25' == result
+
+    def test_datefmt_long_default(self, past_datetime: datetime):
+        am = ArticlesModel(**dict())
+        fmt = am.datefmt_long
+        result = past_datetime.strftime(fmt)
+        assert '2012/04/25' == result
+
+    def test_datefmt_full_default(self, past_datetime: datetime):
+        am = ArticlesModel(**dict())
+        fmt = am.datefmt_full
+        result = past_datetime.strftime(fmt)
+        assert '2012/04/25 13:26' == result
