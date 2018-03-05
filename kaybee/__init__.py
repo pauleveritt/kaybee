01b9eec1943738ed2651387029e79ecc39e2ec7a
Paul Everitt <pauleveritt@me.com>
2018-03-05 14:21:24 -0500

    @WIP Let's release 0.1.5

--- kaybee/__init__.py
+++ kaybee/__init__.py
@@ -7,7 +7,7 @@ from kaybee.app import kb
 from kaybee.plugins.events import EventAction, SphinxEvent
 from kaybee.plugins.settings.model import KaybeeSettings
 
-__version__ = '0.1.5.dev0'
+__version__ = '0.1.5'
 __title__ = "kaybee"
 
 
