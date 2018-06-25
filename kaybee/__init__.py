95dc8e8ac63fb4fed1a82f29c6f94c06e633bc58
Paul Everitt <pauleveritt@me.com>
2018-06-25 10:06:09 -0400

    @WIP Let's make a release.

--- kaybee/__init__.py
+++ kaybee/__init__.py
@@ -7,7 +7,7 @@ from kaybee.app import kb
 from kaybee.plugins.events import EventAction, SphinxEvent
 from kaybee.plugins.settings.model import KaybeeSettings
 
-__version__ = '0.1.16.dev0'
+__version__ = '0.1.16'
 __title__ = "kaybee"
 
 
